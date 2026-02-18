ccfaf2b3da08fa445e4610eb690f1c3e5841aae8
John <johnbloome@gmail.com>
2026-02-18 12:01:48 -0600

    sync to waj

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/apps/trivia/", // <--- ADD THIS LINE HERE
   server: {
     host: "::",
     port: 8080,
