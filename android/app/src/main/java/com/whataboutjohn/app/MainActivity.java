5c5d66d412a87682abd9df4f83ac34237a0e738a
John <johnbloome@gmail.com>
2026-02-19 17:06:08 -0600

    updated wording

---
+++ android/app/src/main/java/com/whataboutjohn/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.whataboutjohn.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.whataboutjohn.app',
+  appName: 'Chicago Corner Pieces',
+  webDir: 'dist'
+};
+
+export default config;
